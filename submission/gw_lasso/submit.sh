b35045d5fa02b79316fd415d3852d2de312776b7
Yanyu Liang <yanyul@uchicago.edu>
2020-10-03 09:07:30 -0500

    some debugging but still need work

--- submission/gw_lasso/submit.sh
+++ submission/gw_lasso/submit.sh
@@ -5,12 +5,12 @@ mkdir -p logs
 mkdir -p batch_list
 cd batch_list
 
-split -d -l $batchsize $phenolist batch
+split -d -a 3 -l $batchsize $phenolist batch
 
 cd ../
 nowdir=`pwd`
 
 for i in `ls batch_list/`
 do
-  echo qsub -v BATCH=$i -N gw_lasso_$i run.qsub
+  qsub -v BATCH=$i -N gw_lasso_$i run.qsub
 done

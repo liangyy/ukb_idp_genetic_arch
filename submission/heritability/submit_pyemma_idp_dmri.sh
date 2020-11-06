c466509e696e33344d107f688587d387426ff430
Yanyu Liang <yanyul@uchicago.edu>
2020-11-06 13:52:42 -0600

    added new ridge

---
+++ submission/heritability/submit_pyemma_idp_dmri.sh
@@ -0,0 +1,9 @@
+runscript=../../methods/heritability/run_pyemma.sh
+grm=/vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/subset_genotypes/IDP_HM3_finalPheno.merged_all.maf_gt_0.05
+idp=/vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/regress_out_idp_pcs/2020-05-18_final-phenotypes.cleaned_up_dMRI.parquet
+outdir=/vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/heritability
+
+nametag=pyemma.idp_dmri
+
+
+screen -dmS $nametag bash -c "bash $runscript $idp $grm $outdir/$nametag"

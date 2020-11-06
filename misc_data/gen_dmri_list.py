f4b86183d7d1a83a09f31f110988b3e94c20cf4f
Yanyu Liang <yanyul@uchicago.edu>
2020-11-06 14:17:38 -0600

    added lasso for dmri

---
+++ misc_data/gen_dmri_list.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+output = '/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/dmri_list.txt'
+
+e = pd.read_parquet('/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/2020-05-18_final-phenotypes.cleaned_up_dMRI.parquet')
+mylist = list(e.columns)
+
+with open(output, 'w') as f:
+    for i in mylist:
+        f.write(i + '\n')
---
+++ submission/gw_lasso/dmri/submit_en.sh
@@ -0,0 +1,26 @@
+phenolist=/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/dmri_list.txt
+batchsize=2
+
+mkdir -p logs_en
+mkdir -p batch_list_en
+cd batch_list_en
+
+split -d -a 3 -l $batchsize $phenolist batch
+
+cd ../
+nowdir=`pwd`
+
+for i in `ls batch_list_en/`
+do
+  if [[ -f logs_en/run_$i.out ]]
+  then
+    tmp=`cat logs_en/run_$i.log | tail -n 1 | grep 'failed\|kill\|Errno' | wc -l`
+    if [[ $tmp = 1 ]]
+    then
+      qsub -v BATCH=$i -N gw_en_$i run_en.qsub
+    fi
+  else
+    # :
+    qsub -v BATCH=$i -N gw_en_$i run_en.qsub
+  fi
+done

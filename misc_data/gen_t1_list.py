530ce7aa7eda0eb1fa75d685cdbd4ff2993d2f90
Yanyu Liang <yanyul@uchicago.edu>
2020-11-15 10:22:24 -0600

    added t1 submit

---
+++ misc_data/gen_t1_list.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+output = '/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/t1_list.txt'
+
+e = pd.read_parquet('/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/2020-05-18_final-phenotypes.cleaned_up_T1.parquet')
+mylist = list(e.columns[1:])
+
+with open(output, 'w') as f:
+    for i in mylist:
+        f.write(i + '\n')
---
+++ submission/gw_lasso/t1/submit_lasso.sh
@@ -0,0 +1,26 @@
+phenolist=/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/t1_list.txt
+batchsize=2
+
+mkdir -p logs_lasso
+mkdir -p batch_list_lasso
+cd batch_list_lasso
+
+split -d -a 3 -l $batchsize $phenolist batch
+
+cd ../
+nowdir=`pwd`
+
+for i in `ls batch_list_lasso/`
+do
+  if [[ -f logs_lasso/run_$i.out ]]
+  then
+    tmp=`cat logs_lasso/run_$i.log | tail -n 1 | grep 'failed\|kill\|Errno' | wc -l`
+    if [[ $tmp = 1 ]]
+    then
+      qsub -v BATCH=$i -N gw_lasso_$i run_lasso.qsub
+    fi
+  else
+    :
+    # qsub -v BATCH=$i -N gw_lasso_$i run_lasso.qsub
+  fi
+done

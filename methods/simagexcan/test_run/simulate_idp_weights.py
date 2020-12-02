4ee0862347833e709bfe832356a615d7fd90acf0
Yanyu Liang <yanyul@uchicago.edu>
2020-12-01 22:51:17 -0600

    fixed some issues

--- methods/simagexcan/test_run/simulate_idp_weights.py
+++ methods/simagexcan/test_run/simulate_idp_weights.py
@@ -5,7 +5,7 @@ file_in = sys.argv[1]
 file_out = sys.argv[2]
 
 df = pd.read_parquet(file_in)
-df = df[ df.chr.isin([21, 22]) ].reset_index(drop=True)
+df = df[ df.chr.isin(['21', '22']) ].reset_index(drop=True)
 
 df.to_parquet(file_out)
 

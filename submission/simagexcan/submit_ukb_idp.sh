238ea4e468d39e5d27f2d28039a78542491ef577
Yanyu Liang <yanyul@uchicago.edu>
2020-12-07 11:50:56 -0600

    minor

--- submission/simagexcan/submit_ukb_idp.sh
+++ submission/simagexcan/submit_ukb_idp.sh
@@ -1,10 +1,21 @@
 # ARGS: middle name of the config (tailored to ukb idp cohort)
 
-for i in `seq 1 5`
+for i in `seq 1 22`
 do 
   if (( $i < 7)); then nbatch=200; fi
   if (( $i < 17 & $i > 6 )); then nbatch=100; fi
   if (( $i < 23 & $i > 16 )); then nbatch=50; fi
-  echo qsub -v NBATCH=$nbatch,CHR=$i,NAME=$1 run_geno_cov.qsub
-  qsub -v NBATCH=$nbatch,CHR=$i,NAME=$1 run_geno_cov.qsub
+  filename=logs/run_geno_cov_${1}_$i.log
+  if [[ -f $filename ]]
+  then
+    ff=`cat logs/run_geno_cov_${1}_$i.log |tail -n 1|grep shadow`
+    if [[ ! -z $ff ]]
+    then
+      echo qsub -v NBATCH=$nbatch,CHR=$i,NAME=$1 run_geno_cov.qsub
+      qsub -v NBATCH=$nbatch,CHR=$i,NAME=$1 run_geno_cov.qsub
+    fi
+  else
+    echo qsub -v NBATCH=$nbatch,CHR=$i,NAME=$1 run_geno_cov.qsub
+    qsub -v NBATCH=$nbatch,CHR=$i,NAME=$1 run_geno_cov.qsub
+  fi
 done

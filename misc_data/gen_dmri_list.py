b1071da638895887ccf9ee2425820976c3a4b54c
Yanyu Liang <yanyul@uchicago.edu>
2020-12-10 23:00:48 -0600

    added post process for lasso train

--- misc_data/gen_dmri_list.py
+++ misc_data/gen_dmri_list.py
@@ -1,10 +1,18 @@
+import re, yaml
 import pandas as pd
 
 output = '/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/dmri_list.txt'
+output_yaml = '/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/dmri_list.yaml'
 
 e = pd.read_parquet('/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/2020-05-18_final-phenotypes.cleaned_up_dMRI.parquet')
 mylist = list(e.columns[1:])
 
+dict_ = {}
 with open(output, 'w') as f:
     for i in mylist:
+        dict_[re.sub('-', 'x', i)] = i 
         f.write(i + '\n')
+    
+with open(output_yaml, 'w') as f:
+    yaml.dump(dict_, f)
+    
---
+++ submission/gw_lasso/dmri/post_train.sh
@@ -0,0 +1,22 @@
+outdir=/scratch/t.cri.yliang/ukb_idp/gw_elastic_net_dmri
+tmplist=batch_list.txt
+if [[ -f $tmplist ]]
+then
+  rm $tmplist
+fi
+
+for l in `ls batch_list_en/`
+do
+  echo "$outdir/$l.gw_lasso.weights.tsv.gz" >> $tmplist
+done
+
+conda activate ukb_idp
+export PYTHONPATH=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/misc-tools/pyutil
+
+rename_yaml=/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/regress_out_idp_pcs/dmri_list.yaml
+
+python post_process_training.py \
+  --model_list $tmplist \
+  --rename_yaml $rename_yaml \
+  --output $outdir/gw_lasso_beta.parquet
+  

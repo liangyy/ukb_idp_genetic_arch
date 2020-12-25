2c10ebafab083ebb23004f671f8387ac4b05d580
Yanyu Liang <yanyul@uchicago.edu>
2020-12-24 20:40:48 -0600

    still running t1

--- submission/gw_lasso/t1_2nd_round/submit_en.sh
+++ submission/gw_lasso/t1_2nd_round/submit_en.sh
@@ -1,10 +1,8 @@
 # args1: nametag
 nametag=$1
-phenopre=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/ukb_idp_genetic_arch/submission/gw_lasso/t1_2nd/list_misc/$nametag/pheno_list
+phenopre=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/ukb_idp_genetic_arch/submission/gw_lasso/t1_2nd_round/list_misc/$nametag/pheno_list
 batchsize=2
 filein=/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/second_round_idp_preprocessing/$nametag.parquet
-
-python /gpfs/data/im-lab/nas40t2/yanyul/GitHub/ukb_idp_genetic_arch/misc_data/gen_gw_lasso_list.py --input $filein --output_prefix $phenopre
 
 mkdir -p logs_en
 mkdir -p batch_list_en
@@ -14,6 +12,11 @@ mkdir -p configs
 mkdir -p logs_en/$nametag
 mkdir -p batch_list_en/$nametag
 mkdir -p list_misc/$nametag
+
+if [[ ! -f $phenopre.txt ]]
+then
+  python /gpfs/data/im-lab/nas40t2/yanyul/GitHub/ukb_idp_genetic_arch/misc_data/gen_gw_lasso_list.py --input $filein --output_prefix $phenopre
+fi
 
 cat config.en.yaml | sed "s#PLACEHOLDER#$nametag#g" > configs/config.$nametag.yaml
 
@@ -31,10 +34,10 @@ do
     tmp=`cat logs_en/$nametag/run_$i.log | tail -n 1 | grep 'failed\|kill\|Errno' | wc -l`
     if [[ $tmp = 1 ]]
     then
-      echo qsub -v BATCH=$i -N gw_en_$i run_en.qsub
+      qsub -v BATCH=$i,NAMETAG=$nametag -N gw_en_$i run_en.qsub
     fi
   else
-    # :
-    echo qsub -v BATCH=$i -N gw_en_$i run_en.qsub
+    :
+    # qsub -v BATCH=$i,NAMETAG=$nametag -N gw_en_$i run_en.qsub
   fi
 done

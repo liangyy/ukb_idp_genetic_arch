a69d1085628675417286bd298d5323bfada665fe
Yanyu Liang <yanyul@uchicago.edu>
2020-12-27 10:58:26 -0600

    still on hm3 lookup

--- submission/preprocessing/gen_lookup_table/run.sh
+++ submission/preprocessing/gen_lookup_table/run.sh
@@ -1,1 +1,13 @@
-export PYTHONPATH=/vol/bmd/yanyul/GitHub/misc-tools//liftover_snp:/vol/bmd/yanyul/GitHub/misc-tools//pyutil:$PYTHONPATH;         python gen_lookup_table.py             --input /vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/hapmap3_eur_snplist/hapmap3_eur.no_ambiguity.pop_CEU_maf_0.01.txt             --input_map /vol/bmd/yanyul/data/hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.map             --liftover_chain /vol/bmd/yanyul/data/chain_files/hg18ToHg19.over.chain.gz             --output /vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/hapmap3_eur_snplist/hapmap3_eur.no_ambiguity.pop_CEU_maf_0.01.lookup_table.tsv.gz
+# environment setup
+source /vol/bmd/yanyul/miniconda3/etc/profile.d/conda.sh
+conda activate py3
+
+infile=/vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/hapmap3_eur_snplist/hapmap3_eur_pop_CEU_maf_0.01_with_qc.txt
+outfile=/vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/hapmap3_eur_snplist/hapmap3_eur_pop_CEU_maf_0.01_with_qc.lookup_table.tsv.gz 
+
+export PYTHONPATH=/vol/bmd/yanyul/GitHub/misc-tools/liftover_snp:/vol/bmd/yanyul/GitHub/misc-tools/pyutil:$PYTHONPATH
+python gen_lookup_table.py \
+  --input $infile \
+  --input_map /vol/bmd/yanyul/data/hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.map \
+  --liftover_chain /vol/bmd/yanyul/data/chain_files/hg18ToHg19.over.chain.gz \
+  --output $outfile

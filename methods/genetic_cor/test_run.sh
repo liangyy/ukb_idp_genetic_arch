6cf95f942b4ff8728bd49923c01f1558c6f5ea66
Yanyu Liang <yanyul@uchicago.edu>
2021-01-11 09:33:39 -0600

    some fixes

--- methods/genetic_cor/test_run.sh
+++ methods/genetic_cor/test_run.sh
@@ -4,7 +4,8 @@ conda activate ldsc
 
 ldscdir=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/ldsc
 gwas=/gpfs/data/im-lab/nas40t2/Data/SummaryResults/imputed_gwas_hg38_1.1/imputed_pgc.scz2.txt.gz
-qtldir=trans_qtl.dmri.original.all_covar.w_pc.chr{chr_num}
+snpmeta=/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/subset_genotypes/IDP_HM3_finalPheno.merged_all.bim
+qtldir=/scratch/t.cri.yliang/ukb_idp/idp_gwas_2nd/trans_qtl.dmri.original.all_covar.w_pc.chr{chr_num}
 scoredir=/gpfs/data/im-lab/nas40t2/yanyul/data/eur_w_ld_chr
 outdir=test_run_out
 
@@ -17,16 +18,20 @@ then
     --a1 effect_allele \
     --a2 non_effect_allele \
     --p pvalue \
-    --signed-sumstats zscore \
+    --signed-sumstats zscore,0 \
+    --snp variant_id \
     --N-col sample_size \
     --out $outdir/gwas_formatted \
     --merge-alleles $scoredir/w_hm3.snplist
 fi
 
-python $ldscdir/ldsc.py \
+echo python $ldscdir/ldsc.py \
   --rg $outdir/gwas_formatted.sumstats.gz,$qtldir/IDP-25361.parquet,$qtldir/IDP-25203.parquet \
   --ref-ld-chr $scoredir/ \
   --w-ld-chr $scoredir/ \
-  --out $outdir/rg_out
+  --out $outdir/rg_out \
+  --load-parquet \
+  --snp-meta $snpmeta \
+  --sample-size 24409
   
   

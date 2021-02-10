44225bf1e16284bdafdfb71cccd04936fadbf3d0
Yanyu Liang <yanyul@uchicago.edu>
2021-02-10 15:05:50 -0600

    added indiv 2nd

---
+++ submission/prediction_2nd/merge_t1_ridge.sh
@@ -0,0 +1,18 @@
+#PBS -S /bin/bash
+#PBS -l walltime=6:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=4gb
+#PBS -e logs/merge_t1_ridge.err
+#PBS -o logs/merge_t1_ridge.out
+
+
+source ~/.bash_profile
+source ~/.bashrc
+conda activate ukb_idp
+
+python /gpfs/data/im-lab/nas40t2/yanyul/GitHub/ukb_idp_genetic_arch/methods/prediction/post_merge.py \
+  --input_pattern /gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/predicted_idp/pred_idp.t1_ridge_2nd.chr{chr_num}.parquet \
+  --indiv_col indiv \
+  --output /gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/predicted_idp/pred_idp.t1_ridge_2nd.parquet \
+  > ${PBS_O_WORKDIR}/logs/merge_t1_ridge.log 2>&1
+  
---
+++ submission/prediction_2nd/submit_cri_t1_ridge.sh
@@ -0,0 +1,23 @@
+prs_parquet=/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/idp_models_2nd/t1.scaled.all_covar.w_pc.gw_ridge_beta.parquet
+NAME=t1_ridge_2nd
+logdir=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/ukb_idp_genetic_arch/submission/prediction/logs
+
+for i in `seq 1 22`
+do 
+  if [[ -f $logdir/pred_cri_t1_ridge_2nd_$i.log ]]
+  then
+    e=`cat $logdir/pred_cri_t1_ridge_2nd_$i.log|tail -n 1 | grep 'sqlite3\|ModuleNotFoundError'` 
+    if [[ ! -z $e ]]
+    then 
+      echo $i
+    fi
+  else
+    echo $i
+  fi
+done > tmp
+
+for i in `cat tmp`
+do 
+  echo qsub -v prs_parquet=$prs_parquet,NAME=$NAME,CHR=$i -N $i.pred_ridge_t1 pred_cri.qsub  
+done
+rm tmp

cdf360b801a5ff71b37f58e1b556bba083b21307
Yanyu Liang <yanyul@uchicago.edu>
2021-04-01 07:42:07 -0500

    added pred 3rd

---
+++ submission/prediction/submit_cri_t1_en_3rd.sh
@@ -0,0 +1,23 @@
+prs_parquet=/scratch/t.cri.yliang/ukb_idp/idp_models_3rd/third_round_t1.gw_elastic_net_beta.parquet
+NAME=t1_en_3rd
+logdir=/gpfs/data/im-lab/nas40t2/yanyul/GitHub/ukb_idp_genetic_arch/submission/prediction/logs
+
+for i in `seq 1 22`
+do 
+  if [[ -f $logdir/pred_cri_t1_en_3rd_$i.log ]]
+  then
+    e=`cat $logdir/pred_cri_t1_en_3rd_$i.log|tail -n 1 | grep 'sqlite3\|ModuleNotFoundError'` 
+    if [[ ! -z $e ]]
+    then 
+      echo $i
+    fi
+  else
+    echo $i
+  fi
+done > tmp
+
+for i in `cat tmp`
+do 
+  qsub -v prs_parquet=$prs_parquet,NAME=$NAME,CHR=$i -N $i.pred_en_t1 pred_cri.qsub  
+done
+rm tmp

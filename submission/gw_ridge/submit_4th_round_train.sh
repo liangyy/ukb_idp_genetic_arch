b40051286c91d8692e11f94766259f4cfa29cd4c
Yanyu Liang <yanyul@uchicago.edu>
2021-04-07 18:21:26 -0500

    added ridge 4th

---
+++ submission/gw_ridge/submit_4th_round_train.sh
@@ -0,0 +1,12 @@
+datadir=/vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/fourth_round_idp_preprocessing
+outdir=/vol/bmd/yanyul/UKB/ukb_idp_genetic_arch/gw_ridge_4th
+
+mkdir -p $outdir
+
+tags="fourth_round.dmri_no_pc fourth_round.dmri_w_pc fourth_round.t1_no_pc fourth_round.t1_w_pc"
+    
+for nametag in $tags
+do
+  screen -dmS $nametag bash train_generic.screen $datadir/$nametag.parquet $outdir/$nametag.gw_ridge_beta.parquet train_$nametag
+done  
+

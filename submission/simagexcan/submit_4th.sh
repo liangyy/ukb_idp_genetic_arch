6c5598d4f6abfe7ee60a56034975debb1aacf89a
Yanyu Liang <yanyul@uchicago.edu>
2021-04-11 11:33:20 -0500

    added sbxcan 4th (leftover)

---
+++ submission/simagexcan/submit_4th.sh
@@ -0,0 +1,12 @@
+# ARGS1: nametag
+# ARGS2: phenotype_list (config middle name)
+# ARGS3: processor per node
+nametag=$1
+pheno_list=$2
+ppn=$3
+
+mkdir -p configs
+
+cat config.$pheno_list.yaml | sed "s#PLACEHOLDER#$nametag#g" > configs/config.${pheno_list}_$nametag.yaml
+
+qsub -v NAME=${pheno_list}_$nametag,PPN=$ppn -l nodes=1:ppn=$ppn -N ${pheno_list}_$nametag run_simgx_4th.qsub

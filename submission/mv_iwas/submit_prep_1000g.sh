b65fb4933e8b0fef023fe82c28c1e0f38f454ea2
Yanyu Liang <yanyul@uchicago.edu>
2021-05-25 17:48:47 -0500

    need screen

--- submission/mv_iwas/submit_prep_1000g.sh
+++ submission/mv_iwas/submit_prep_1000g.sh
@@ -1,8 +1,8 @@
 mkdir -p logs
 for i in `seq 1 22`
 do
-  # export CHR=$i 
-  # screen -dmS chr$i bash prep_1000g_genotype.sbatch
+  export CHR=$i 
+  screen -dmS chr$i bash prep_1000g_genotype.qsub
   # sbatch --export=CHR=$i prep_1000g_genotype.sbatch
   qsub -v CHR=$i prep_1000g_genotype.qsub
 done

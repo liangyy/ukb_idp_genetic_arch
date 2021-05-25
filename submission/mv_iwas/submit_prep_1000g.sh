c654cd2ab4e70b5048392c82187169ba652907ee
Yanyu Liang <yanyul@uchicago.edu>
2021-05-25 16:59:52 -0500

    some updates

--- submission/mv_iwas/submit_prep_1000g.sh
+++ submission/mv_iwas/submit_prep_1000g.sh
@@ -3,5 +3,6 @@ for i in `seq 1 22`
 do
   # export CHR=$i 
   # screen -dmS chr$i bash prep_1000g_genotype.sbatch
-  sbatch --export=CHR=$i prep_1000g_genotype.sbatch
+  # sbatch --export=CHR=$i prep_1000g_genotype.sbatch
+  qsub -v CHR=$i prep_1000g_genotype.qsub
 done

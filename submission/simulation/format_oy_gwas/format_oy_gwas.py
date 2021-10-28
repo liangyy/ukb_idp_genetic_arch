4e5574a2b0ae5c3e87eccf468ed501065b902693
Yanyu Liang (UChicago) <yanyuluchicago@Yanyus-MacBook-Pro.local>
2021-10-28 07:44:34 -0700

    added gwas formatting

---
+++ submission/simulation/format_oy_gwas/format_oy_gwas.py
@@ -0,0 +1,65 @@
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog='format_oy_gwas.py', 
+        description='''
+            Format GWAS output from `gw_qtl` to txt.gz files
+        ''')
+    parser.add_argument('--pheno_list', help='''
+        The list of phenotype file tag name to extract
+    ''')
+    parser.add_argument('--input_pattern', help='''
+        The parquet file pattern with {chr_num} and {pheno} as wildcards
+    ''')
+    parser.add_argument('--snp_bim_pattern', help='''
+        Corresponding SNP BIM file pattern with {chr_num} as wildcard
+    ''')
+    parser.add_argument('--output_pattern', help='''
+        Output TXT.GZ file pattern with {pheno} as wildcard 
+    ''')
+    args = parser.parse_args()
+ 
+    import logging, time, sys, os
+    # configing util
+    logging.basicConfig(
+        level = logging.INFO, 
+        stream = sys.stderr, 
+        format = '%(asctime)s  %(message)s',
+        datefmt = '%Y-%m-%d %I:%M:%S %p')
+    import pandas as pd
+    
+    logging.info('Loading phenotype list')
+    pheno_list = []
+    with open(args.pheno_list, 'r') as f:
+        for i in f:
+            i = i.strip()
+            pheno_list.append(i)
+    logging.info('There are {len(pheno_list)} phenotypes to work on')
+    
+    logging.info('Loading SNP meta info')
+    df_snp = []
+    for i in range(1, 23):
+        fn = args.snp_bim_pattern.format(chr_num=i)
+        tmp = pd.read_csv(fn, sep='\t', header=None)
+        tmp.columns = ['chr', 'variant_id', 'ph', 'pos', 'ref', 'alt']
+        df_snp.append(tmp)
+    df_snp = pd.concat(df_snp, axis=0)
+    
+    logging.info('Loading GWAS')
+    for pheno in pheno_list:
+        logging.info('-> On phenotype = {pheno}')
+        df_gwas = []
+        for i in range(1, 23):
+            fn = args.input_pattern.format(chr_num=i, pheno=pheno)
+            tmp = pd.read_parquet(fn)
+            tmp = pd.merge(
+                tmp, 
+                df_snp[['variant_id', 'chr', 'pos', 'ref', 'alt']], 
+                on='variant_id')
+            df_gwas.append(tmp)
+        df_gwas = pd.concat(df_gwas, axis=0)
+        df_gwas.to_csv(
+            args.output_pattern.format(pheno=pheno), 
+            compression='gzip',
+            sep='\t')
+    
+    
---
+++ submission/simulation/format_oy_gwas/submit.sh
@@ -0,0 +1,21 @@
+rand_seeds=(
+  1
+  2
+  3
+  4
+  5)
+
+gwas_dir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/run_gwas"
+geno_dir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/split_genotypes"
+outdir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/format_oy_gwas"
+mkdir -p "${outdir}"
+
+for rand in "${rand_seeds[@]}"; do
+  ls "${gwas_dir}/trans_qtl.param1.group_group2.rand_${rand}.oy.chr22" | \
+    sed 's/.parquet//g' > "logs/rand_${rand}.txt"
+  echo qsub -v \
+    PHENO_LIST="logs/rand_${rand}.txt",\
+INPUT_PATTERN="${gwas_dir}/trans_qtl.param1.group_group2.rand_${rand}.oy.chr{chr_num}/{pheno}.parquet",\
+SNP_BIM_PATTERN="${geno_dir}/group2.chr{chr_num}.bim",\
+OUTPUT_PATTERN="${outdir}/{pheno}.txt.gz"
+done

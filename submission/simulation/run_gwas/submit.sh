d8fb8a39aa9f4e14f1964baf71c2a78fbb902e4d
Yanyu Liang <yanyul@uchicago.edu>
2021-10-27 22:39:52 -0500

    almost finished

--- submission/simulation/run_gwas/submit.sh
+++ submission/simulation/run_gwas/submit.sh
@@ -5,7 +5,11 @@ rand_seeds=(
   4
   5)
 
+geno_dir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/split_genotypes"
 outdir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/run_gwas"
+geno1="${geno_dir}/group1.chr{chr_num}"
+geno2="${geno_dir}/group2.chr{chr_num}"
+
 mkdir -p configs
 mkdir -p "${outdir}"
 
@@ -14,13 +18,27 @@ for rand in "${rand_seeds[@]}"; do
   nametag1="param1.group_group1.rand_${rand}.omed"
   # oy GWAS on group 2
   nametag2="param1.group_group2.rand_${rand}.oy"
-  
+ 
   cat config.param1.yaml | sed "s#PLACEHOLDER#${nametag1}#g" > "configs/config.${nametag1}.yaml"
   cat config.param1.yaml | sed "s#PLACEHOLDER#${nametag2}#g" > "configs/config.${nametag2}.yaml"
   
   for i in $(seq 1 22); do
-    echo qsub -v CHR=$i,NAME="${nametag1}",outdir="${outdir}" -N "${i}_${nametag1}_gwas" run.qsub
-    echo qsub -v CHR=$i,NAME="${nametag2}",outdir="${outdir}" -N "${i}_${nametag2}_gwas" run.qsub
+    if [[ -f "logs/${nametag1}.${i}.out" ]]; then
+      kk="$(cat "logs/${nametag1}.${i}.out" | grep Exit | tail -n 1 | grep 1)"
+      if [[ ! -z "${kk}" ]]; then
+        qsub -v CHR=$i,NAME="${nametag1}",OUTDIR="${outdir}",GENO_PATTERN="${geno1}.bed" -N "${i}_${nametag1}_gwas" run.qsub
+      fi
+    else
+      qsub -v CHR=$i,NAME="${nametag1}",OUTDIR="${outdir}",GENO_PATTERN="${geno1}.bed" -N "${i}_${nametag1}_gwas" run.qsub
+    fi
+    if [[ -f "logs/${nametag2}.${i}.out" ]]; then
+      kk="$(cat "logs/${nametag2}.${i}.out" | grep Exit | tail -n 1 | grep 1)"
+      if [[ ! -z "${kk}" ]]; then
+        qsub -v CHR=$i,NAME="${nametag2}",OUTDIR="${outdir}",GENO_PATTERN="${geno2}.bed" -N "${i}_${nametag2}_gwas" run.qsub
+      fi
+    else
+      qsub -v CHR=$i,NAME="${nametag2}",OUTDIR="${outdir}",GENO_PATTERN="${geno2}.bed" -N "${i}_${nametag2}_gwas" run.qsub
+    fi
   done
 done
 
--- submission/simulation/train_ridge/submit_param1.sh
+++ submission/simulation/train_ridge/submit_param1.sh
@@ -3,7 +3,8 @@ rand_seeds=(
   2
   3
   4
-  5)
+  5
+)
 groups=(1 2)
 param_tag="param1"
 pheno_dir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/simulate_phenotypes"
@@ -14,13 +15,13 @@ mkdir -p "${outdir}"
 for group in "${groups[@]}"; do
   for rand in "${rand_seeds[@]}"; do 
     geno_pattern="${geno_dir}/group${group}.chr{chr_num}"
-    tag="${param_tag}_group${group}.rand_${rand}"
+    tag="${param_tag}.group_group${group}.rand_${rand}"
     pheno_parquet="${pheno_dir}/${tag}.omed.parquet"
-    echo qsub -v \
+    qsub -v \
       GENO_PATTERN="${geno_pattern}",\
 PHENO_PARQUET="${pheno_parquet}",\
 TAG="${tag}",\
-CACHE_TAG="${geno_pattern}/group${group}",\
+CACHE_TAG="${geno_dir}/group${group}",\
 OUTDIR="${outdir}" \
       run.qsub
   done

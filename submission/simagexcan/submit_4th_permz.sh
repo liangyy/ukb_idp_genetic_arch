8f5a95f55a1097e92adacaf5769925bae993af32
Yanyu Liang <yanyul@uchicago.edu>
2021-12-06 17:14:40 -0600

    fixed minor issue

--- submission/simagexcan/submit_4th_permz.sh
+++ submission/simagexcan/submit_4th_permz.sh
@@ -7,18 +7,20 @@ phenolist=$2
 mkdir -p configs
 
 for pheno in $(cat "${phenolist}"); do
-  if [[ $pheno == "Neuroticism_CTG" || $pheno == "AlcDep_PGC_2018" ]]; then
-    extra="zscore=zscore"
+  if [[ $pheno != "Neuroticism_CTG" && $pheno != "AlcDep_PGC_2018" ]]; then
+    extra="effect_size=effect_size"
+  else
+    extra=""
   fi
   ff="logs/run_simgx_4th_permz_${configname}_${pheno}.log"
   if [[ -f "${ff}" ]]; then
     tmp=$(cat "${ff}" | tail -n 2 | grep "failed\|kill\|Errno\|File" | wc -l)
     if [[ $tmp != 0 ]]; then
-      echo qsub -v CONFIGNAME="${configname}",GWASNAME="${pheno}",EXTRA="" \
+      qsub -v CONFIGNAME="${configname}",GWASNAME="${pheno}",EXTRA="${extra}" \
         -N ${configname}_${pheno} run_simgx_4th_permz.qsub
     fi
   else
-    echo qsub -v CONFIGNAME="${configname}",GWASNAME="${pheno}",EXTRA="" \
+    qsub -v CONFIGNAME="${configname}",GWASNAME="${pheno}",EXTRA="${extra}" \
       -N ${configname}_${pheno} run_simgx_4th_permz.qsub
   fi
   

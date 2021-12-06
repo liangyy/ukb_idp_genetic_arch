48bf31893b018fcb3227a328de1e0c3de7f1b586
Yanyu Liang <yanyul@uchicago.edu>
2021-12-06 12:21:13 -0600

    added check

--- submission/simagexcan/submit_4th_permz.sh
+++ submission/simagexcan/submit_4th_permz.sh
@@ -7,7 +7,16 @@ phenolist=$2
 mkdir -p configs
 
 for pheno in $(cat "${phenolist}"); do
-  echo qsub -v CONFIGNAME="${configname}",GWASNAME="${pheno}" \
-    -N ${configname}_$nametag run_simgx_4th_permz.qsub
+  ff="logs/run_simgx_4th_permz_${configname}_${pheno}.log"
+  if [[ -f "${ff}" ]]; then
+    tmp=$(cat "${ff}" | tail -n 1 | grep "failed\|kill\|Errno\|File" | wc -l)
+    if [[ $tmp = 1 ]]; then
+      echo qsub -v CONFIGNAME="${configname}",GWASNAME="${pheno}" \
+        -N ${configname}_${pheno} run_simgx_4th_permz.qsub
+    fi
+  else
+    echo qsub -v CONFIGNAME="${configname}",GWASNAME="${pheno}" \
+      -N ${configname}_${pheno} run_simgx_4th_permz.qsub
+  fi
 done
 

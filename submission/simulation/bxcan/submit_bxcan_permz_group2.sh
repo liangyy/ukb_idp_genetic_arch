36d749d30705dc28815470ef2d78a9717ccd25ee
Yanyu Liang (UChicago) <yanyuluchicago@Yanyus-MacBook-Pro.local>
2021-12-06 17:14:55 -0800

    still on bxcan permz for simulation

--- submission/simulation/bxcan/submit_bxcan_permz_group2.sh
+++ submission/simulation/bxcan/submit_bxcan_permz_group2.sh
@@ -5,20 +5,25 @@ rand_seeds=(
   4
   5)
   
-nametag="group2_bxcan_permz"
+nametag="permz"
+geno_cov="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/bxcan/group1.geno_cov.chr{chr_num}.banded.npz"
 
 outdir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/bxcan_permz"
 gwas_dir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/format_oy_gwas"
 
 for rand in "${rand_seeds[@]}"; do
+  idpname="param1.group_group1.rand_${rand}.ridge"
+  tagname="${nametag}_${rand}"
   for gwas in $(ls "${gwas_dir}"/group2.rand_"${rand}".oy.* | \
     sed "s#${gwas_dir}/group2.##g" | \
     sed 's/.txt.gz//g'); do
-    qsub \
-      -v CONFIGNAME="${nametag}",\
+    echo qsub \
+      -v TAGNAME="${tagname}",\
 GWASNAME="${gwas}",\
+GENO_COV="${geno_cov}",\
+IDP_WEIGHT="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/train_ridge/${idpname}.parquet",\
 OUTDIR="${outdir}" \
-      -N "group2_bxcan_${nametag}" \
+      -N "${nametag}" \
       run_bxcan_permz.qsub
   done
 done

77325afd8e7ec5c47fb4cfb65824235d3934de1b
Yanyu Liang (UChicago) <yanyuluchicago@Yanyus-MacBook-Pro.local>
2021-12-13 14:32:37 -0800

    added gwas formating

--- submission/simulation/format_oy_gwas/submit.sh
+++ submission/simulation/format_oy_gwas/submit.sh
@@ -12,15 +12,19 @@ outdir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/format_oy_gwas"
 mkdir -p "${outdir}"
 mkdir -p logs
 
+rand0=2000
+kk=0
+
 for rand in "${rand_seeds[@]}"; do
-  ls "${gwas_dir}/trans_qtl.param1.group_group2.rand_${rand}.oy.chr22" | \
-    sed 's/.parquet//g' > "logs/rand_${rand}.txt"
-  qsub -v \
-    PHENO_LIST="logs/rand_${rand}.txt",\
-NAME="rand_${rand}",\
-INPUT_PATTERN="${gwas_dir}/trans_qtl.param1.group_group2.rand_${rand}.oy.chr{chr_num}/{pheno}.parquet",\
+  (( kk2 = rand0 + rand + 20 ))  
+  ls "${gwas_dir}/trans_qtl.param1.group_group2.rand_${kk2}.oy.chr22" | \
+    sed 's/.parquet//g' > "logs/rand_${kk2}.txt"
+  echo qsub -v \
+    PHENO_LIST="logs/rand_${kk2}.txt",\
+NAME="rand_${kk2}",\
+INPUT_PATTERN="${gwas_dir}/trans_qtl.param1.group_group2.rand_${kk2}.oy.chr{chr_num}/{pheno}.parquet",\
 SNP_BIM_PATTERN="${geno_dir}/group2.chr{chr_num}.bim",\
-OUTPUT_PATTERN="${outdir}/group2.rand_${rand}.oy.{pheno}.txt.gz",\
+OUTPUT_PATTERN="${outdir}/group2.rand_${kk2}.oy.{pheno}.txt.gz",\
 SAMPLE_SIZE="${sample_size}" \
     run.qsub
 done

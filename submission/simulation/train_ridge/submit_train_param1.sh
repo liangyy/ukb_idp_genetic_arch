fabe82ecde09fe0ffd825fa66aba60847c9f344a
Yanyu Liang (UChicago) <yanyuluchicago@Yanyus-MacBook-Pro.local>
2021-12-13 11:27:44 -0800

    added run ridge

--- submission/simulation/train_ridge/submit_train_param1.sh
+++ submission/simulation/train_ridge/submit_train_param1.sh
@@ -12,10 +12,14 @@ geno_dir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/split_genotypes"
 outdir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/train_ridge"
 mkdir -p "${outdir}"
 
+rand0=2000
+kk=0
+
 for group in "${groups[@]}"; do
   for rand in "${rand_seeds[@]}"; do 
+    (( kk = rand0 + rand + group * 10 ))
     geno_pattern="${geno_dir}/group${group}.chr{chr_num}"
-    tag="${param_tag}.group_group${group}.rand_${rand}"
+    tag="${param_tag}.group_group${group}.rand_${kk}"
     pheno_parquet="${pheno_dir}/${tag}.omed.parquet"
     qsub -v \
       GENO_PATTERN="${geno_pattern}",\

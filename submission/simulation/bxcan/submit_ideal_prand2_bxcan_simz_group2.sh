90f1838a12f5a03b273a549164ac8a5c8da9ed8c
Yanyu Liang (UChicago) <yanyuluchicago@Yanyus-MacBook-Pro.local>
2021-12-15 13:53:19 -0800

    unfor added prank2

--- submission/simulation/bxcan/submit_ideal_prand2_bxcan_simz_group2.sh
+++ submission/simulation/bxcan/submit_ideal_prand2_bxcan_simz_group2.sh
@@ -12,21 +12,36 @@ geno_cov="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/bxcan/group1.geno_
 outdir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/ideal_prand2_bxcan_simz"
 gwas_dir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/format_oy_gwas"
 
+rand0=2000
+kk2=0
+
 for rand in "${!rand_seeds[@]}"; do
   rand_idp="${rand_seeds[${rand}]}"
+  (( kk2 = rand0 + rand + 20 ))
   idpname="param1.rand_${rand_idp}.snp_effect"
   tagname="${nametag}_${rand}"
-  for gwas in $(ls "${gwas_dir}"/group2.rand_"${rand}".oy.* | \
+  for gwas in $(ls "${gwas_dir}"/group2.rand_"${kk2}".oy.* | \
     sed "s#${gwas_dir}/group2.##g" | \
     sed 's/.txt.gz//g'); do
-    qsub \
-      -v TAGNAME="${tagname}",\
+    doit="1"
+    fn="logs/run_bxcan_permz_${tagname}_${gwas}.log"
+    if [[ -f "${fn}" ]]; then
+      doit=""
+      tmp=$(cat "${fn}" | tail -n 1 | grep 'Error\|Quit')
+      if [[ ! -z "${tmp}" ]]; then
+        doit="1"
+      fi
+    fi
+    if [[ ! -z "${doit}" ]]; then
+      qsub \
+        -v TAGNAME="${tagname}",\
 GWASNAME="${gwas}",\
 GENO_COV="${geno_cov}",\
 IDP_WEIGHT="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/simulate_phenotypes/${idpname}.parquet",\
 OUTDIR="${outdir}" \
-      -N "${nametag}" \
-      run_ideal_bxcan_simz.qsub
+        -N "${nametag}" \
+        run_ideal_bxcan_simz.qsub
+    fi
   done
 done
 

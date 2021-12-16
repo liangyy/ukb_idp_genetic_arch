c041f7eb8bcb16cf30df92fc953ce97fc94e0993
Yanyu Liang (UChicago) <yanyuluchicago@Yanyus-MacBook-Pro.local>
2021-12-15 21:30:35 -0800

    on param2

---
+++ submission/simulation/bxcan/submit_param2_ideal_bxcan_permz_group2.sh
@@ -0,0 +1,46 @@
+rand_seeds=(
+  1
+  2
+  3
+  4
+  5)
+  
+nametag="ideal_permz_param2"
+geno_cov="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/bxcan/group1.geno_cov.chr{chr_num}.banded.npz"
+
+outdir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/ideal_bxcan_permz_param2"
+gwas_dir="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/format_oy_gwas_param2"
+
+rand0=2000
+kk2=0
+
+for rand in "${rand_seeds[@]}"; do
+  (( kk2 = rand0 + rand + 20 ))
+  idpname="param2.rand_${rand}.snp_effect"
+  tagname="${nametag}_${rand}"
+  for gwas in $(ls "${gwas_dir}"/group2.rand_"${kk2}".oy.* | \
+    sed "s#${gwas_dir}/group2.##g" | \
+    sed 's/.txt.gz//g'); do
+    doit="1"
+    fn="logs/run_bxcan_permz_${tagname}_${gwas}.log"
+    if [[ -f "${fn}" ]]; then
+      doit=""
+      tmp=$(cat "${fn}" | tail -n 1 | grep 'Error\|Quit')
+      if [[ ! -z "${tmp}" ]]; then
+        doit="1"
+      fi
+    fi
+    if [[ ! -z "${doit}" ]]; then
+      qsub \
+        -v TAGNAME="${tagname}",\
+GWASNAME="${gwas}",\
+GENO_COV="${geno_cov}",\
+IDP_WEIGHT="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/simulate_phenotypes_param2/${idpname}.parquet",\
+OUTDIR="${outdir}" \
+        -N "${nametag}" \
+        run_ideal_bxcan_permz.qsub
+    fi
+  done
+done
+
+

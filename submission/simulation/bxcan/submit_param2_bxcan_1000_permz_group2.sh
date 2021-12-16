52d5c814331e3cccab4fe5ec02572d07b634d4c0
Yanyu Liang (UChicago) <yanyuluchicago@Yanyus-MacBook-Pro.local>
2021-12-16 08:21:08 -0800

    on param2 to run onsplit weights for bxcan

--- submission/simulation/bxcan/submit_param2_bxcan_1000_permz_group2.sh
+++ submission/simulation/bxcan/submit_param2_bxcan_1000_permz_group2.sh
@@ -4,7 +4,12 @@ rand_seeds=(
   3
   4
   5)
-  
+h2s=(
+  0.3
+  0.5
+  0.7
+  0.9
+)
 nametag="permz_1000_param2"
 geno_cov="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/bxcan_1000/group1.geno_cov.chr{chr_num}.banded.npz"
 
@@ -23,6 +28,16 @@ for rand in "${rand_seeds[@]}"; do
   for gwas in $(ls "${gwas_dir}"/group2.rand_"${kk2}".oy.* | \
     sed "s#${gwas_dir}/group2.##g" | \
     sed 's/.txt.gz//g'); do
+    h2=""
+    for h2i in "${h2s[@]}"; do
+      tmp=$(echo "${gwas}" | grep "h2_${h2i}")
+      if [[ ! -z "${tmp}" ]]; then
+        h2="${h2i}"
+      fi
+    done
+    if [[ -z "${h2}" ]]; then
+      echo "WARNING: SOMETHING WRONG FOR ${gwas}"
+    fi
     doit="1"
     fn="logs/run_bxcan_permz_${tagname}_${gwas}.log"
     if [[ -f "${fn}" ]]; then
@@ -37,7 +52,7 @@ for rand in "${rand_seeds[@]}"; do
         -v TAGNAME="${tagname}",\
 GWASNAME="${gwas}",\
 GENO_COV="${geno_cov}",\
-IDP_WEIGHT="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/train_ridge_param2/${idpname}.parquet",\
+IDP_WEIGHT="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/train_ridge_param2/split.${idpname}.h2_${h2}.parquet",\
 OUTDIR="${outdir}" \
         -N "${nametag}" \
         run_bxcan_permz.qsub
--- submission/simulation/bxcan/submit_param2_bxcan_permz_group2.sh
+++ submission/simulation/bxcan/submit_param2_bxcan_permz_group2.sh
@@ -4,6 +4,12 @@ rand_seeds=(
   3
   4
   5)
+h2s=(
+  0.3
+  0.5
+  0.7
+  0.9
+)
   
 nametag="permz_param2"
 geno_cov="/gpfs/data/im-lab/nas40t2/yanyul/ukb_idp/simulation/bxcan/group1.geno_cov.chr{chr_num}.banded.npz"
@@ -23,6 +29,16 @@ for rand in "${rand_seeds[@]}"; do
   for gwas in $(ls "${gwas_dir}"/group2.rand_"${kk2}".oy.* | \
     sed "s#${gwas_dir}/group2.##g" | \
     sed 's/.txt.gz//g'); do
+    h2=""
+    for h2i in "${h2s[@]}"; do
+      tmp=$(echo "${gwas}" | grep "h2_${h2i}")
+      if [[ ! -z "${tmp}" ]]; then
+        h2="${h2i}"
+      fi
+    done
+    if [[ -z "${h2}" ]]; then
+      echo "WARNING: SOMETHING WRONG FOR ${gwas}"
+    fi
     doit="1"
     fn="logs/run_bxcan_permz_${tagname}_${gwas}.log"
     if [[ -f "${fn}" ]]; then

be781e794edacfea16017cafd7244a14bdab9e8c
Yanyu Liang <yanyu018@gmail.com>
2024-04-17 23:35:15 -0700

    updates for new submit 202404

---
+++ misc_data/supplementary_materials_4th/update_table_s8_twasinf.R
@@ -0,0 +1,10 @@
+# setwd('~/Documents/repo/github/ukb_idp_genetic_arch/misc_data/supplementary_materials_4th/')
+# update table s8
+old <- read.csv('~/Downloads/Table_S8.xlsx - ..csv')
+new <- readRDS("s_bxcan_twasinf/dataframe_full.sbxcan_twasinf.rds")
+
+library(dplyr)
+cols.to.update <- c('pval_corrected', 'z_corrected')
+cols.join <- c('IDP', 'model', 'phenotype')
+old <- left_join(old, new[, c(cols.join, cols.to.update)], by = cols.join)
+write.csv(old %>% select(-z_adj_perm_null, -pval_adj_perm_null), 'Table_S8-twasinf.csv')
